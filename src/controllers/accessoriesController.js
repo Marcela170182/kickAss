d4c7e99e61610cec2eff6909d9e9780348897757
JeffersonCorreiax <correia_jefferson@outlook.com>
2022-10-24 23:21:58 -0300

    página de acessorios dinamica

--- src/controllers/accessoriesController.js
+++ src/controllers/accessoriesController.js
@@ -1,7 +1,18 @@
 const path = require("path");
+const fs = require('fs');
+
+const accessoriesFilePath = path.join(__dirname, '../database/accessories.json');
+const accessories = JSON.parse(fs.readFileSync(accessoriesFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
+
 const accessoriesController = {
     index : (req, res) => {
-        res.render('acessorios');
+        
+        
+        
+        return  res.render('acessorios', {accessories});
     }
 }
 
